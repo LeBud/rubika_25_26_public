212b7749fbf1301a84d7e9eb3d0c654c6c48a54f
LeBud <c.huyghe@rubika-edu.com>
2026-01-20 17:26:48 +0100

    Tresheold 2 fini

--- MyEngine/rubika_2025/DrunkardWalk.cpp
+++ MyEngine/rubika_2025/DrunkardWalk.cpp
@@ -18,33 +18,36 @@ DrunkardWalk::DrunkardWalk(unsigned sizeX, unsigned sizeY, unsigned iteration, u
 }
 
 bool DrunkardWalk::Generate() {
-    sf::Vector2<unsigned>* pos = new sf::Vector2<unsigned>(
+    sf::Vector2<unsigned>* originalPos = new sf::Vector2<unsigned>(
         Globals::GetInstance()->GetRandomMgr()->GetInstance(0)->RandUInt(0,SizeX-1),
         Globals::GetInstance()->GetRandomMgr()->GetInstance(0)->RandUInt(0,SizeY-1));
+    
+    for (unsigned i = 0; i < Iteration; i++) {
+        sf::Vector2<unsigned>* pos = originalPos;
+        unsigned cell = pos->y * SizeY + originalPos->x;
+        Cells.at(cell) = 1;
 
-    unsigned cell = pos->y * SizeY + pos->x;
-    Cells.at(cell) = 1;
-
-    for (unsigned i = 0; i < Distance * Iteration; i++) {
-        unsigned dir = Globals::GetInstance()->GetRandomMgr()->GetInstance(0)->RandUInt(0,3);
-        switch (dir) {
-            case 0:
-                pos->y++;
-            break;
-            case 1:
-                pos->x++;
-            break;
-            case 2:
-                pos->y--;
-            break;
-            case 3:
-                pos->x--;
-            break;
-            default:
-                return false;
+        for (unsigned i = 0; i < Distance; i++) {
+            unsigned dir = Globals::GetInstance()->GetRandomMgr()->GetInstance(0)->RandUInt(0,3);
+            switch (dir) {
+                case 0:
+                    pos->y++;
+                break;
+                case 1:
+                    pos->x++;
+                break;
+                case 2:
+                    pos->y--;
+                break;
+                case 3:
+                    pos->x--;
+                break;
+                default:
+                    return false;
+            }
+            cell = pos->y * SizeY + pos->x;
+            Cells[cell] = 1;
         }
-        cell = pos->y * SizeY + pos->x;
-        Cells[cell] = 1;
     }
 
     GenerateTexture(Texture);
